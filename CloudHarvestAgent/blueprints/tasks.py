4a6cb59f8e5d48a78cd9a4174964da489290a902
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-11-27 13:33:14 -0500

    added terminate and status endpoints

--- CloudHarvestAgent/blueprints/tasks.py
+++ CloudHarvestAgent/blueprints/tasks.py
@@ -1,20 +1,54 @@
-from blueprints.base import HarvestBlueprint
+from base import HarvestBlueprint
 from flask import Response, jsonify, request
 
 # Blueprint Configuration
 tasks_blueprint = HarvestBlueprint(
     'tasks_bp', __name__,
-    url_prefix='/agent'
+    url_prefix='/tasks'
 )
 
-@tasks_blueprint.route(rule='shutdown', methods=['GET'])
-def terminate() -> Response:
+@tasks_blueprint.route(rule='shutdown/<task_id>', methods=['GET'])
+def terminate(task_id: str) -> Response:
+    """
+    Stops the processing of a TaskChain based on its ID.
+
+    Arguments:
+        task_id (str): The ID of the TaskChain to stop.
+
+    Returns:
+        A Response object containing the result of the operation.
+    """
+
     from ..app import CloudHarvestAgent
 
-    CloudHarvestAgent.job_queue.terminate()
+    task_object = CloudHarvestAgent.job_queue.get(task_id)
+
+    if task_object is None:
+        return jsonify({'error': 'Task not found.'})
+
+    else:
+        task_object.terminate()
+        return jsonify({'message': 'Task terminated.'})
 
 
-@tasks_blueprint.route(rule='status', methods=['GET'])
-def status() -> Response:
-    pass
+@tasks_blueprint.route(rule='status/<task_id>', methods=['GET'])
+def status(task_id: str) -> Response:
+    """
+    Retrieves the status of a TaskChain based on its ID.
 
+    Arguments:
+        task_id (str): The ID of the TaskChain to retrieve the status of.
+
+    Returns:
+        A Response object containing the status of the TaskChain
+    """
+
+    from ..app import CloudHarvestAgent
+
+    task_object = CloudHarvestAgent.job_queue.get(task_id)
+
+    if task_object is None:
+        return jsonify({'error': 'Task not found.'})
+
+    else:
+        return jsonify(task_object.status)
