938f956875bdde32b0469f814452a2c2f5b1467b
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-11-27 13:43:03 -0500

    added new blueprints

--- CloudHarvestAgent/blueprints/__init__.py
+++ CloudHarvestAgent/blueprints/__init__.py
@@ -1,2 +1,10 @@
-from .agent import agent_blueprint
-from .queue import queue_blueprint
+"""
+__init__.py is a special Python file that is executed when a directory is imported as a package.
+
+This file is used to import all the blueprints into the Flask app.
+"""
+
+from agent import agent_blueprint
+from home import home_blueprint
+from queue import queue_blueprint
+from tasks import tasks_blueprint
