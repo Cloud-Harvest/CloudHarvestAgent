1ce6fd11937a1612399eee59aa96b100c07b1863
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-11-27 13:39:39 -0500

    adde reload code comments and changed imports

--- CloudHarvestAgent/blueprints/agent.py
+++ CloudHarvestAgent/blueprints/agent.py
@@ -1,4 +1,8 @@
-from blueprints.base import HarvestBlueprint, logger
+"""
+The agent blueprint is responsible for managing the Flask agent.s
+"""
+
+from base import HarvestBlueprint
 from flask import Response, jsonify, request
 from json import loads
 from logging import getLogger
@@ -11,8 +15,20 @@ agent_blueprint = HarvestBlueprint(
     url_prefix='/agent'
 )
 
+@agent_blueprint.route(rule='reload', methods=['GET'])
+def reload() -> Response:
+    """
+    Reloads the agent's configuration, allowing configurations to be updated without stopping the agent.
+    """
+    # TODO: Implement this method
+    pass
+
 @agent_blueprint.route(rule='shutdown', methods=['GET'])
 def shutdown() -> Response:
+    """
+    Shuts down the agent.
+    """
+
     from ..app import CloudHarvestAgent
 
     request_json = loads(request.get_json())
